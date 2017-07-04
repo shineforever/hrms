5b7cbb5c09c2b51abc2367e8c0cbf7f17fe1f183
shine_forever <shine_forever@yeah.net>
2017-07-04 22:13:54 +0800

    staff模块修改

--- staff/models.py
+++ staff/models.py
@@ -26,11 +26,30 @@ class Position(models.Model):
     职位
     """
     name = models.CharField(max_length=20, verbose_name=u'职位名称')
-    department = models.ForeignKey(Department,name=u'所属部门',null=True,on_delete=models.SET_NULL)
+    department = models.ForeignKey(Department,name=u'所属部门',blank=True,null=True,on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = u"职位名称"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
 
 
 class Staff(models.Model):
     """
     职员信息
     """
-    pass
+    name = models.CharField(max_length=10,verbose_name=u'姓名')
+    gender = models.CharField(choices=(('male', u'男'), ('female', u'女')), default='male', verbose_name=u'性别',
+                              max_length=6)
+    position = models.ForeignKey(Position,verbose_name=u'所属职位',blank=True,null=True,on_delete=models.SET_NULL)
+    identity_card = models.CharField(max_length=18, verbose_name=u'身份证', null=True,blank=True,unique=True)
+    mobile = models.CharField(max_length=11, verbose_name=u'手机号码', null=True)
+
+    class Meta:
+        verbose_name = u"职员信息"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
