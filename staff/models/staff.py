89fe969e61dd26fffed77d0c4b964fb9c3420ab6
BgFist <1565771886@qq.com>
2017-07-24 17:46:34 +0800

    .

--- staff/models/staff.py
+++ staff/models/staff.py
@@ -35,7 +35,7 @@ class Staff(models.Model):
 
     IDENTITY_TYPE_CHOICE = (
         (1, u'身份证'),
-        (2, u'居民证')
+        (2, u'护照')
     )
 
     GENDER_CHOICE = (
@@ -43,20 +43,55 @@ class Staff(models.Model):
         (2, u'女')
     )
 
-    # 工号，根据id计算
+    MARRIAGE_STATE = (
+        (1, u'未婚'),
+        (2, u'已婚'),
+        (3, u'已婚已育')
+    )
 
+    BIRTH_TYPE = (
+        (1, u'公历'),
+        (2, u'农历'),
+    )
+
+    # 工号，根据id计算
+    e_number = ''
+    # 一级部门，根据职位计算
+    dep1 = 1
+    # 二级部门，根据职位计算
+    dep2 = 2
+    # 工龄，根据入职时间计算
+    e_age = 3.9
+
+    # 基本信息
     name = models.CharField(max_length=10, verbose_name=u'姓名')
     identity_type = models.SmallIntegerField(choices=IDENTITY_TYPE_CHOICE, verbose_name=u'证件类型', default=1)
     identity_card = models.CharField(max_length=18, verbose_name=u'证件号', null=True, blank=True, unique=True)
-    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1, verbose_name=u'性别')
-    position = models.ForeignKey(Position, verbose_name=u'所属职位', blank=True, null=True, on_delete=models.SET_NULL)
     mobile = models.CharField(max_length=11, verbose_name=u'手机号码', null=True)
-    education = models.SmallIntegerField(choices=EDUCATION_CHOICE, verbose_name=u'学历', default=1)
+    marriage = models.SmallIntegerField(choices=MARRIAGE_STATE, verbose_name=u'婚姻状况', default=1)
+    birth_type = models.SmallIntegerField(choices=BIRTH_TYPE, verbose_name=u'生日类型', default=1)
+    birthday = models.DateField(verbose_name=u'出生日期', null=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1, verbose_name=u'性别')
+    people = models.CharField(max_length=11, verbose_name=u'民族', default=u'汉族')
+    birthplace = models.CharField(max_length=10, verbose_name=u'籍贯', null=True)
+
+    # 教育状况
+    education = models.SmallIntegerField(choices=EDUCATION_CHOICE, verbose_name=u'最高学历', default=1)
     political_status = models.SmallIntegerField(choices=POLITICAL_CHOICE, verbose_name=u'政治面貌', default=1)
-    staff_status = models.SmallIntegerField(choices=STAFF_CHOICE, verbose_name=u'职员状态', default=1)
-    staff_type = models.SmallIntegerField(choices=STAFF_TYPE_CHOICE, verbose_name=u'职员类型', default=1)
-    qq = models.CharField(verbose_name=u'QQ号', max_length=15, null=True, blank=True)
-    email = models.EmailField(verbose_name=u'电子邮箱', null=True, blank=True, max_length=100)
+
+    # 在职信息
+    staff_status = models.SmallIntegerField(choices=STAFF_CHOICE, verbose_name=u'员工状态', default=1)
+    staff_type = models.SmallIntegerField(choices=STAFF_TYPE_CHOICE, verbose_name=u'员工类型', default=1)
+    position = models.ForeignKey(Position, verbose_name=u'职位', blank=True, null=True, on_delete=models.SET_NULL)
+    is_master = models.BooleanField(verbose_name=u'是否是部门管理者', default=False)
+    e_email = models.EmailField(verbose_name=u'工作邮箱', null=True, blank=True, max_length=100)
+
+    # 联系方式
+    qq = models.CharField(verbose_name=u'QQ', max_length=15, null=True, blank=True)
+    wxid = models.CharField(verbose_name=u'微信', max_length=15, null=True, blank=True)
+    email = models.EmailField(verbose_name=u'个人邮箱', null=True, blank=True, max_length=100)
+    contact_man = models.CharField(verbose_name=u'紧急联系人', max_length=10, null=True)
+    contact_mobile = models.CharField(verbose_name=u'紧急联系人电话', max_length=11, null=True)
 
     class Meta:
         verbose_name = u"职员"
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 
@@ -12,6 +13,7 @@ class UserProfile(AbstractUser):
     管理员信息
     """
     mobile = models.CharField(max_length=11, null=True, blank=True)
+
     # image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
 
     class Meta:
